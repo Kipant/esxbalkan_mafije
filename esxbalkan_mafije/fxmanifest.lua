65e6004ae696927ef1c2116832c8bb18dfde8f5f
Kipant <130110016+Kipant@users.noreply.github.com>
2023-04-26 23:50:12 +0200

    Add files via upload

---
+++ esxbalkan_mafije/fxmanifest.lua
@@ -0,0 +1,38 @@
+fx_version 'cerulean'
+game 'gta5'
+version "1.9.3"
+
+lua54 'yes'
+
+shared_scripts {
+	'@es_extended/locale.lua',
+	'prevod/*',
+	'config.lua'
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/*'
+}
+
+client_script 'client/*'
+
+dependencies {
+	'es_extended',
+	'esxbalkan_addonaccount',
+	'esxbalkan_addoninventory',
+	'esxbalkan_datastore',
+	'esxbalkan_society',
+	'oxmysql'
+}
+
+ui_page 'html/index.html'
+
+files {
+    "html/*.png",
+    "html/index.html",
+    "html/style.css",
+    "html/jos-neki-js.js.js",
+    "html/app.js",
+	"html/assets/*.png"
+}
